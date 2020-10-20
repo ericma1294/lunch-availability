c9042d5b1d9a527f6aa40674a08a5fb27efe33c7
Eric Ma <ericma1294@gmail.com>
2020-10-20 19:44:02 +0900

    second commit

---
+++ build/precache-manifest.7cf59ac60388c3d509cad5cfd4cfc246.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4506b80d76d74d26c7797faffa49eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9a32118f41b70052b46",
+    "url": "/static/css/main.3209e7ea.chunk.css"
+  },
+  {
+    "revision": "66dcceadc7db78bc3ce3",
+    "url": "/static/js/2.16271a66.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.16271a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a32118f41b70052b46",
+    "url": "/static/js/main.b2537867.chunk.js"
+  },
+  {
+    "revision": "c3b65c2da9e1aa7dee88",
+    "url": "/static/js/runtime-main.94fb465c.js"
+  },
+  {
+    "revision": "94142f350b8e8bf4f213b026e8b5825a",
+    "url": "/static/media/no.94142f35.jpg"
+  },
+  {
+    "revision": "37ce1ec8d93ca4b0b1690d270aa4f893",
+    "url": "/static/media/yes.37ce1ec8.jpeg"
+  }
+]);
